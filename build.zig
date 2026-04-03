378315e3c50a173ceb3901344ad95f8a70eee1f2
Vinicius Mayrink <vmayrink12@gmail.com>
2026-04-03 16:20:26 -0300

    chore(build): uses `llvm`
    This allows debugging via `lldb`.

--- build.zig
+++ build.zig
@@ -6,6 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "gwin",
+        .use_llvm = true,
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
